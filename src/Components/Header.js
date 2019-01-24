7993a9e7fd4cc9716f81e9fea4015a6edf68e599
Spence Johnson <spencejohnson@Spences-MacBook-Air.local>
2019-01-24 09:44:50 -0700

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+let Header = () => {
+  return (
+    <div className='App-header'>
+      
+      <h3>There is nothing worth doing that is not profoundly innovative</h3>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Biology</li>
+        <li>Tech</li>
+        <li>Science</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
